bf2f9bfb60de2973344824025d8b93b3f5aec2f4
Bohatynska Oksana <98977553+Bohatynska@users.noreply.github.com>
2023-09-29 15:33:51 +0100

    fixed onDelete (again)

--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -1,10 +1,10 @@
 import { ContactWrap, ContactUl } from './ContactList.styled';
 import ContactListItem from 'Components/ContactItem/ContactItem';
-const ContactList = ({ contacts }) => {
+const ContactList = ({ contacts, onDelete }) => {
   return (
     <ContactWrap>
       <ContactUl>
-        {contacts.map(({ id, name, number, onDelete }) => (
+        {contacts.map(({ id, name, number }) => (
           <ContactListItem
             key={id}
             name={name}
